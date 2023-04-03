c0c8870044046c201777352a0aab806d89144a06
Arbaz Khan <125760210+Arbazkhan00@users.noreply.github.com>
2023-04-04 00:34:33 +0500

    4th Question is to commit

---
+++ Question 4/index.js
@@ -0,0 +1,3 @@
+var famous_person = "Mother Teresa";
+var massage = "".concat(famous_person, " once said,Spread love everywhere you go. Let no one ever come to you without leaving happier");
+console.log(massage);
---
+++ Question 4/index.ts
@@ -0,0 +1,3 @@
+let famous_person="Mother Teresa";
+let massage=`${famous_person} once said,Spread love everywhere you go. Let no one ever come to you without leaving happier`;
+console.log(massage);
