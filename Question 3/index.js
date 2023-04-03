b5510c0066908876edefa88cbd3e5a4e99584b58
Arbaz Khan <125760210+Arbazkhan00@users.noreply.github.com>
2023-04-04 00:30:31 +0500

    third Question is to commit

---
+++ Question 3/index.js
@@ -0,0 +1,2 @@
+var person_nam = "Nelson Mandela";
+console.log("".concat(person_nam, " once said ,The greatest glory in living lies not in never falling, but in rising every time we fall."));
---
+++ Question 3/index.ts
@@ -0,0 +1,2 @@
+let person_nam="Nelson Mandela";
+console.log(`${person_nam} once said ,The greatest glory in living lies not in never falling, but in rising every time we fall.`);
