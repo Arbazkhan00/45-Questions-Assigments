c0cc5a6f235806f4517993b0d673b11c42aa6a4a
Arbaz Khan <125760210+Arbazkhan00@users.noreply.github.com>
2023-04-05 23:31:18 +0500

    Question 11

---
+++ Question 11/index.js
@@ -0,0 +1,5 @@
+var persons_names = ["Arbaz khan", "Adil", "Haris", "Mohsin"];
+for (var _i = 0, persons_names_1 = persons_names; _i < persons_names_1.length; _i++) {
+    var person = persons_names_1[_i];
+    console.log("Hello,".concat(person, " your are looking great today"));
+}
---
+++ Question 11/index.ts
@@ -0,0 +1,4 @@
+let persons_names:string[]=["Arbaz khan","Adil","Haris","Mohsin"];
+for(const person of persons_names ){
+console.log
+(`Hello,${person} your are looking great today`);}
