97df8d079c20b6f167183b50c52fb44dda79923b
Arbaz Khan <125760210+Arbazkhan00@users.noreply.github.com>
2023-04-06 18:10:07 +0500

    Delay function

---
+++ Delay Assigments/delay.js
@@ -0,0 +1,7 @@
+console.log("Hi,my name is arbaz khan:");
+setTimeout(function () {
+    console.log("Im live in murree");
+}, 2000);
+setTimeout(function () {
+    console.log("My Father name is ikram ullah");
+}, 500);
---
+++ Delay Assigments/delay.ts
@@ -0,0 +1,8 @@
+console.log("Hi,my name is arbaz khan:");
+setTimeout(()=>{
+   console.log("Im live in murree")
+},2000);
+setTimeout(() => {
+    console.log("My Father name is ikram ullah");
+    
+}, 500);
